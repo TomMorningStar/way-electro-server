fb78835a2af08e42e0c233d1eb702b529856364e
Tamerlan <tamerlan.musaev@mail.ru~>
2025-04-24 23:35:12 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors'); 
 
 const app = express();
 const port = 3000;
 
 const allRoutes = require('./routes');
+
+
+app.use(cors()); 
 
 app.use(express.json());
 app.use(allRoutes);
