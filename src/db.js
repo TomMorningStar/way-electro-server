02924ac10da921850fbbbfb9152a697fdb3f9635
Tamerlan <tamerlan.musaev@mail.ru~>
2025-04-24 17:33:16 +0300

    add checker

--- src/db.js
+++ src/db.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log("PG_HOST:", process.env.PG_HOST); // Должно быть aws-0-eu-central-1.pooler.supabase.com
+if (!process.env.PG_HOST) {
+  throw new Error("PG_HOST не загружен!");
+}
+
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
