2d94c82b39d7084abe47740ae9feba1c1f7c1d18
Tamerlan <tamerlan.musaev@mail.ru~>
2025-05-17 17:12:39 +0300

    fix_add all_brands route

--- src/controllers/all_brands/all_brands.js
+++ src/controllers/all_brands/all_brands.js
@@ -5,7 +5,7 @@ exports.get_all_brands = async (req, res) => {
   try {
     const result = await pool.query(
       `SELECT *
-       FROM get_all_brands`
+       FROM all_brands`
     );
     res.json(result.rows);
   } catch (err) {
--- src/routes/all_brands.js/all_brands.js
+++ src/routes/all_brands.js/all_brands.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { all_brands } = require('../../controllers/all_brands/all_brands');
 
-router.get('/all_brands', all_brands);
+router.get('/all-brands', all_brands);
 
 module.exports = router;
