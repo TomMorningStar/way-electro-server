3bf93e77af93b27ba4be86f114e19c13dae95cae
Tamerlan <tamerlan.musaev@mail.ru~>
2025-07-21 00:47:40 +0300

    redisign

--- src/routes/circuit_breakers/circuit_breakers_list.js
+++ src/routes/circuit_breakers/circuit_breakers_list.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { get_circuit_breakers_list } = require('../../controllers/circuit_breakers/circuit_breakers_list');
 
-router.get('/get_circuit_breakers_list/type/:type_id/brand/:brand_id', get_circuit_breakers_list);
+// router.get('/get_circuit_breakers_list/type/:type_id/brand/:brand_id', get_circuit_breakers_list);
+router.get('/get_circuit_breakers_list', get_circuit_breakers_list);
 
 module.exports = router;
